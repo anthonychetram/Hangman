b2169b4356b6b24d8ac87014f0e3af2b9a9ebdb3
anthony Chetram <anthonychetram28@gmail.com>
2025-10-15 10:42:23 -0400

    Choose Random Word

---
+++ hangman.js
@@ -0,0 +1,3 @@
+var POSSIBLE_WORDS = ["Bruh", "JavaScript", "HTML", "CSS", "Python", "Java", "Booty", "Ruby", "Swift", "Kotlin"];
+var randomIndex = (Math.random() * POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS[randomIndex];

fc0cc5793b64b234cc3e7a8c0e93600dec84adfe
anthony Chetram <anthonychetram28@gmail.com>
2025-10-20 10:06:21 -0400

    Function for new Game

--- hangman.js
+++ hangman.js
@@ -1,3 +1,13 @@
 var POSSIBLE_WORDS = ["Bruh", "JavaScript", "HTML", "CSS", "Python", "Java", "Booty", "Ruby", "Swift", "Kotlin"];
-var randomIndex = (Math.random() * POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex];
+var word = "";
+
+function newGame() {
+    var randomIndex = (Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS[randomIndex];
+    var clueString = "";
+    for (var i = 0; i < word.length; i++) {
+        clueString += "_ ";
+    }
+    var clue = document.getElementById("clue");
+    clue.innerText = clueString;
+}

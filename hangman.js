6093cbff9028451298d31428ae3c9ebde404bb2e
anthony Chetram <anthonychetram28@gmail.com>
2025-10-20 10:33:55 -0400

    Added Guess Letters

--- hangman.js
+++ hangman.js
@@ -4,10 +4,28 @@ var word = "";
 function newGame() {
     var randomIndex = (Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
+    updatePage();
+}
+function guessLetter() {
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses += letter;
+    updatePage();
+}
+function updatePage() {
     var clueString = "";
     for (var i = 0; i < word.length; i++) {
-        clueString += "_ ";
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLetter) >= 0) {
+            clueString += currentLetter + " ";
+        }
+        else
+            clueString += "_ ";
     }
     var clue = document.getElementById("clue");
-    clue.innerText = clueString;
+    clue.innerHTML = clueString;
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed letters: " + guesses;
+
 }
